c5eacd6e4466659aa3857603a594f5b3734ae177
Nick McMillan <mcmilnick@gmail.com>
2019-03-23 00:34:41 -0700

    trying moving the main html

--- docs/source-files.js
+++
@@ -1,4 +0,0 @@
-var N = null;var sourcesIndex = {};
-sourcesIndex["colored"] = {"name":"","dirs":[],"files":["color.rs","control.rs","lib.rs","style.rs"]};
-sourcesIndex["elma_builder"] = {"name":"","dirs":[],"files":["basic_process.rs","channel.rs","lib.rs","manager.rs","process.rs","reciever_proc.rs","sender_proc.rs","stopwatch.rs"]};
-sourcesIndex["lazy_static"] = {"name":"","dirs":[],"files":["inline_lazy.rs","lib.rs"]};
